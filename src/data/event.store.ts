6ff143f75914321f9ca25bd87a941de01c3ec1a8
Dolu <doludev@protonmail.com>
2024-10-02 14:07:27 +0200

    refactor: remove console.log

--- src/data/event.store.ts
+++ src/data/event.store.ts
@@ -30,8 +30,6 @@ class EventStore extends EventEmitter {
     }
 
     getEventsByFilter(filter: Filter): Event[] {
-        console.log(filter);
-        console.log(this.#events);
         let events = this.#events
             .filter((event) => matchFilter(filter, event))
             .sort((a, b) => a.created_at - b.created_at);
